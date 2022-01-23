f5ea720c92ebb4e8b5ab4b60954984a76cc80892
BusteanHAN <B.Corbean@student.han.nl>
2022-01-23 14:29:53 +0000

    Added timestamp insertion on data

--- THmon.py
+++ THmon.py
@@ -2,10 +2,10 @@
 import sys
 import Adafruit_DHT
 
-from datetime import date
+from datetime import datetime
 
 humidity, temperature = Adafruit_DHT.read_retry(11, 17)
 
 #print (f'Temp: {temperature} C  Humidity: {humidity} %')
-with open(f"{date.today()}.txt", "a") as myfile:
-    myfile.write(f'{temperature},{humidity}\n')
+with open(f"{datetime.now().today()}.txt", "a") as myfile:
+    myfile.write(f'{datetime.now.time()}{temperature},{humidity}\n')
