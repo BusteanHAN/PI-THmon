ebbeff3d7e8208c2f39287ead18aa560c9954e76
BusteanHAN <B.Corbean@student.han.nl>
2022-01-23 14:24:14 +0000

    Updated file appends

--- THmon.py
+++ THmon.py
@@ -8,4 +8,4 @@ humidity, temperature = Adafruit_DHT.read_retry(11, 17)
 
 #print (f'Temp: {temperature} C  Humidity: {humidity} %')
 with open(f"{date.today()}.txt", "a") as myfile:
-    myfile.write(f'{temperature},{humidity}')
+    myfile.write(f'{temperature},{humidity}\n')
