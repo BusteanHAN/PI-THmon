74b520fe171d1fe4da036a64de7d824eaaac416e
BusteanHAN <B.Corbean@student.han.nl>
2022-01-23 14:32:12 +0000

    Fixed missing () on now

--- THmon.py
+++ THmon.py
@@ -8,4 +8,4 @@ humidity, temperature = Adafruit_DHT.read_retry(11, 17)
 
 #print (f'Temp: {temperature} C  Humidity: {humidity} %')
 with open(f"{datetime.now().today()}.txt", "a") as myfile:
-    myfile.write(f'{datetime.now.time()}{temperature},{humidity}\n')
+    myfile.write(f'{datetime.now().time()}{temperature},{humidity}\n')
