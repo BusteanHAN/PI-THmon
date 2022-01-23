31c1b5dcf0941571f9b870c46fd67668065a4433
BusteanHAN <B.Corbean@student.han.nl>
2022-01-23 16:37:04 +0100

    Final version

--- THmon.py
+++ THmon.py
@@ -9,5 +9,5 @@ today = datetime.now()
 humidity, temperature = Adafruit_DHT.read_retry(11, 17)
 
 #print (f'Temp: {temperature} C  Humidity: {humidity} %')
-with open(f'{today.strftime("%Y-%m-%d")}.txt', "a") as myfile:
+with open(f'/home/pi/PI-THmon/{today.strftime("%Y-%m-%d")}.txt', "a") as myfile:
     myfile.write(f'{today.strftime("%H-%M-%S")},{temperature},{humidity}\n')
